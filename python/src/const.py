1ce8595204939e1572dc3c1b7dbe07aa59493276
mdd375 <mdd375@yandex.ru>
2024-02-25 16:16:39 +0400

    убрали секретную информацию :)

--- python/src/const.py
+++ python/src/const.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
+from os import getenv
 
-BASE_URL = "http://10.124.20.57:4000/api/v1"
-TOKEN = "token 3dc7c38c526c10676" "533e1262d6b75c7020a05b9"
+BASE_URL = getenv('BASE_URL')
+TOKEN = getenv('TOKEN')
 
 
 @dataclass
